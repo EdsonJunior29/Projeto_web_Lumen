9e5ed8c00cbc2d0aaa19f3f411cd0056d185b9cd
Edson Junior <edsonjos61@gmail.com>
2022-05-02 15:36:18 -0300

    Corrigindo erro de sintaxe.

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Episodio;
 use Illuminate\Http\Request;
 
-class SeriesController extends Controller
+class EpisodiosController extends Controller
 {
 
     public function index()
